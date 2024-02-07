492b882f05ae31ac7a44debe315e3b571cc575bb
@s_zhsn <s.zhosan@gmail.com>
2024-02-07 21:02:09 +0200

    undo style changes

--- components/RegistrationForm.js
+++ components/RegistrationForm.js
@@ -179,10 +179,10 @@ const styles = StyleSheet.create({
     shadowColor: "#000",
     shadowOpacity: 0.3,
     shadowRadius: 3,
-    /*     shadowOffset: {
+    shadowOffset: {
       width: 0,
       height: 2,
-    }, */
+    },
   },
   error: {
     alignSelf: "center",
